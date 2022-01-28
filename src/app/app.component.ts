c889219224aa5cac89885920ff9283c81f2d6604
ErickGomes2805 <erickdesignerdot@gmail.com>
2022-01-28 17:03:22 -0300

    Pasta-app

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'my-app';
+
+constructor(private http:HttpClient){
+  
+}
+
+  ngOnInit() {
+
+}
+  
+}
